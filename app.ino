df44b9bb91c8f8eb64ade809c88b0bda599e99a7
wrobless <awroblewski91@gmail.com>
2020-04-30 22:33:28 +0200

    Temp and humid publish via MQTT

--- app.ino
+++ app.ino
@@ -10,51 +10,122 @@
 
 const char *ssid = "CellSpot_2.4GHz_DF28";
 const char *password = "Wrobeltoziomal";
+const char *mqtt_server = "192.168.29.244";
 
 DHT dht(INPUTPIN, DHTTYPE);
 
-String readTemperature()
-{
-    float temperature = 0.0;
-    temperature = dht.readTemperature();
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
 
-    if (isnan(temperature))
+float temperature = 0;
+float humidity = 0;
+
+void setup_wifi()
+{
+    delay(10);
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.print(ssid);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED)
     {
-        Serial.println("Failed to read from sensor!");
-        return "--";
+        delay(500);
+        Serial.print(".");
     }
-    else
-    {
-        Serial.print("Temperature: ");
-        Serial.println(temperature);
-    }
-    return String(temperature);
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.print(WiFi.localIP());
 }
 
-String readHumidity()
+void callback(char *topic, byte *message, unsigned int length)
 {
-    float humidity = 0.0;
-    humidity = dht.readHumidity();
+    Serial.print("Message arrived on topic: ");
+    Serial.print(topic);
+    Serial.print(". Message: ");
+    String messageTemp;
 
-    if (isnan(humidity))
+    for (int i = 0; i < length; i++)
     {
-        Serial.println("Failed to read from sensor!");
-        return "--";
+        Serial.print((char)message[i]);
+        messageTemp += (char)message[i];
     }
-    else
+    Serial.println();
+
+    if (String(topic) == "esp32/output")
     {
-        Serial.print("Humidity: ");
-        Serial.println(humidity);
+        Serial.print("Changing output to ");
+        if (messageTemp == "on")
+        {
+            Serial.println("on");
+            digitalWrite(LED, HIGH);
+        }
+        else if (messageTemp == "off")
+        {
+            Serial.println("off");
+            digitalWrite(LED, LOW);
+        }
     }
-    return String(humidity);
+}
+
+void reconnect()
+{
+    while (!client.connected())
+    {
+        Serial.print("Attempting MQTT connection...");
+        if (client.connect("ESP32Client"))
+        {
+            Serial.println("connected");
+            client.subscribe("esp32/output");
+        }
+        else
+        {
+            Serial.print("failed, rc=");
+            Serial.print(client.state());
+            Serial.println(" try again in 5 seconds");
+            delay(5000);
+        }
+    }
 }
 
 void setup()
 {
-
+    Serial.begin(115200);
+    pinMode(LED, OUTPUT);
+    dht.begin();
+    setup_wifi();
+    client.setServer(mqtt_server, 1883);
+    client.setCallback(callback);
 }
 
 void loop()
 {
+    if (!client.connected())
+    {
+        reconnect();
+    }
+    client.loop();
 
+    long now = millis();
+    if (now - lastMsg > 5000)
+    {
+        lastMsg = now;
+
+        temperature = dht.readTemperature();
+        char tempString[8];
+        dtostrf(temperature, 1, 2, tempString);
+        Serial.print("Temperature: ");
+        Serial.println(tempString);
+        client.publish("esp32/temperature", tempString);
+
+        humidity = dht.readHumidity();
+        char humString[8];
+        dtostrf(humidity, 1, 2, humString);
+        Serial.print("Humidity: ");
+        Serial.println(humString);
+        client.publish("esp32/humidity", humString);
+    }
 }
